6fc43ced8ac263706424866a7c2ece665b07028f
Amir Hanna <amir.elkess@gmail.com>
2025-02-13 00:08:17 +0100

    Initial setup

--- PostKidUI/Dashboard.Designer.cs
+++ PostKidUI/Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PostKidUI
 {
-    partial class Form1
+    partial class Dashboard
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(960, 540);
+            Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Dashboard";
+            Text = "PostKid: A clone of Postman";
+            Load += Dashboard_Load;
+            ResumeLayout(false);
         }
 
         #endregion
---
+++ PostKidUI/Dashboard.cs
@@ -0,0 +1,15 @@
+namespace PostKidUI
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

83c48c5f205e0f913b1cdf94bf885825f85023bb
Amir Hanna <amir.elkess@gmail.com>
2025-02-18 23:49:28 +0100

    test commit

--- PostKidLibrary/ApiAccess.cs
+++ PostKidLibrary/ApiAccess.cs
@@ -5,6 +5,8 @@ public class ApiAccess : IApiAccess
 {
     private readonly HttpClient client = new();
 
+    // add support for POST
+
     public async Task<string> callApiAsync(string url, HttpAction action = HttpAction.GET)
     {
         var response = await client.GetAsync(url);
